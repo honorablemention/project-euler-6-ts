a0ae793e55261396c36d4669b68fbd4fa9e80b0e
Steven Schultz <steven_schultz@icloud.com>
2024-05-19 01:01:43 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 /**
  * Project Euler #6
  */
-
+const sum = (a: number, b: number) => a + b;
 const sumOfSquares = (n: number) => 
     Array.from({ length: n + 1 })
         .map((_, idx) => Math.pow(idx, 2))
-        .reduce((prev, curr) => prev + curr, 0);
+        .reduce(sum, 0);
 
 const squareOfSum = (n: number) => 
     Math.pow(
         Array.from({ length: n + 1})
             .map((_, idx) => idx)
-            .reduce((prev, curr) => prev + curr, 0)
+            .reduce(sum, 0)
         , 2
     );
 
